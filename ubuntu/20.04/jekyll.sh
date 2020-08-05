009b169d565ea05fc53467267111a9f0684a29dd
d4rckh <m4dd0x.xxx@gmail.com>
2020-08-05 20:25:09 +0300

    Create jekyll.sh

---
+++ ubuntu/20.04/jekyll.sh
@@ -0,0 +1,12 @@
+# test on:
+# Linux ubuntu 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux 
+# (20.04)
+# requires sudo
+# works with bash, replace .bashrc with .zshrc if you are using zsh
+
+sudo apt-get install ruby-full build-essential zlib1g-dev
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+sudo gem install jekyll bundler
